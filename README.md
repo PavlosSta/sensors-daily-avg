f7b7c04da559b996505d61b0891e0148aca2612a
Pavlos <a3r1k0@gmail.com>
2019-10-09 23:02:01 +0300

    Updated instructions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sensors-daily-avg
 A small python script for calculating the daily averages of Temperature and Humidity from sensors' csv files.
+
+## Usage
+Ubuntu terminal command: python sensors-daily-avg.py data.csv
---
+++ sensors-daily-avg.py
@@ -0,0 +1,44 @@
+import csv
+import sys
+
+with open(sys.argv[1], mode='r') as csv_file:
+    reader = csv.reader(csv_file)
+    line_count = 0
+    count = 0
+    SumTemp = 0
+    SumHumid = 0
+    for row in reader:
+        datetime = row[0]
+        datetimeT = datetime.split()
+        check = False
+        for x in datetimeT:
+            if (check):
+                date = x
+                check = False
+            else:
+                time = x
+                check = True
+        temp = float(row[1])
+        humid = float(row[2])
+        if (line_count == 0):
+            oldDate = date
+            count = 1
+            SumTemp = temp
+            SumHumid = humid
+        else:
+            if (date == oldDate):
+                count = count + 1
+                SumTemp += temp
+                SumHumid += humid
+            else:
+                AvgTemp = SumTemp / count
+                AvgHumid = SumHumid / count
+                print (str(date) + " " + str(AvgTemp) + " " + str(AvgHumid))
+                count = 1
+                oldDate = date
+                SumTemp = temp
+                SumHumid = humid
+        line_count += 1
+    AvgTemp = SumTemp / count
+    AvgHumid = SumHumid / count
+    print (str(date) + " " + str(AvgTemp) + " " + str(AvgHumid))
